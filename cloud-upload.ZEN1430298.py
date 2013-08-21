8571e52ffdfe407ea66abf6e2277ceb403c6e8da
Tom Noonan II <thomas.noonan@rackspace.com>
2013-08-21 16:32:26 -0500

    Added notes for using servicenet

--- cloud-upload.ZEN1430298.py
+++ cloud-upload.ZEN1430298.py
@@ -26,7 +26,8 @@ def upload_file(src, container, cdn_user, cdn_key, region):
         return 1
 
     try:
-        cf = pyrax.connect_to_cloudfiles(region=region)
+        # Set public to False to use ServiceNet (Untested)
+        cf = pyrax.connect_to_cloudfiles(region=region, public=True)
     except Exception, e:
         print "!!!!!!! EXCEPTION: Failed to connect to cloud files !!!!! %s" % e
         return 1
